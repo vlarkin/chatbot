55a89b33749ab1434dcc80dd7df70b7b8bc0efc9
Volodymyr Larkin <volodymyr.larkin@datto.com>
2024-03-26 03:53:18 +0100

    Chatbot version 1.0.0

--- cmd/chatbot.go
+++ cmd/chatbot.go
@@ -1,19 +1,33 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Volodymyr Larkin <vlarkin@gmail.com>
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
 )
+
+var (
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+type Joke struct {
+	Setup     string `json:"setup"`
+	Punchline string `json:"punchline"`
+}
 
 // chatbotCmd represents the chatbot command
 var chatbotCmd = &cobra.Command{
-	Use:   "chatbot",
-	Short: "A brief description of your command",
+	Use:     "chatbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,8 +35,52 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("chatbot called")
+		// create telebot object
+		chatbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		// exit if failed to connect or print status
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		} else {
+			log.Printf("\nChatbot version %s started", appVersion)
+		}
+		// process requests
+		chatbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+			// answer on 'hello' and 'joke' requests
+			switch payload {
+			case "hello":
+				// print hello message
+				err = m.Send(fmt.Sprintf("Hello I'm ChatBot %s!", appVersion))
+			case "joke":
+				// print a joke
+				joke := getRandomPun()
+				err = m.Send(fmt.Sprintf("%s", joke))
+			}
+			return err
+
+		})
+
+		chatbot.Start()
+
 	},
+}
+
+// Get a random pun
+func getRandomPun() string {
+	puns := []Joke{
+		{Setup: "What's orange and sounds like a parrot?", Punchline: "A carrot!"},
+		{Setup: "Why couldn’t the leopard play hide and seek?", Punchline: "Because he was always spotted!"},
+		{Setup: "Why don't scientists trust atoms?", Punchline: "Because they make up everything!"},
+	}
+	rand.Seed(int64(len(puns))) // Seed random number generator
+	randomIndex := rand.Intn(len(puns))
+	return fmt.Sprintf("%s\n%s", puns[randomIndex].Setup, puns[randomIndex].Punchline)
 }
 
 func init() {
